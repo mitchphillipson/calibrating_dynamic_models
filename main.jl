d411efca0ab07fb95549495f913aab9dbf406743
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-09-18 08:28:14 -0500

    Improve Formatting

--- main.jl
+++ main.jl
@@ -1,3 +1,7 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
 using MPSGE
 using DataFrames
 using PlotlyJS
@@ -12,7 +16,7 @@ include("scalar_models.jl")
 
 
 
-
+## Static Model ##
 
 static_data = StaticData(
     200, # Y0
@@ -30,30 +34,46 @@ set_value!(static_model[:TAX], 0.1);
 solve!(static_model)
 generate_report(static_model)
 
-
-
+#######################################
+## Interest Rate Determined by Model ##
+#######################################
 
 d1_data = dynamic_data_1(static_data);
 d1_model = dynamic_scalar_model(d1_data);
 solve!(d1_model, cumulative_iteration_limit=0)
 set_value!.(d1_model[:TAX][2010:2100], 0.1);
 solve!(d1_model)
-df1 = dyn_model_report(d1_model, d1_data; value_name = :dyn1)
+df1 = dyn_model_report(d1_model, d1_data; value_name = :Fixed_Rate)
+
+
+############################################
+## Interest Rate Given, adjust investment ##
+############################################
 
 d2_data = dynamic_data_2(static_data);
 d2_model = dynamic_scalar_model(d2_data);
 solve!(d2_model, cumulative_iteration_limit=0)
 set_value!.(d2_model[:TAX][2010:2100], 0.1);
 solve!(d2_model)
-df2 = dyn_model_report(d2_model, d2_data; value_name = :dyn2)
+df2 = dyn_model_report(d2_model, d2_data; value_name = :Adjust_Investment)
 
 
+############################################
+## Interest Rate Given, adjust capital #####
+############################################
 d3_data = dynamic_data_3(static_data);
 d3_model = dynamic_scalar_model(d3_data);
 solve!(d3_model, cumulative_iteration_limit=0)
 set_value!.(d3_model[:TAX][2010:2100], 0.1);
 solve!(d3_model)
-df3 = dyn_model_report(d3_model, d3_data; value_name = :dyn3)
+df3 = dyn_model_report(d3_model, d3_data; value_name = :Adjust_Capital)
+
+
+
+###############
+## Reporting ##
+###############
+
 
 df = vcat(
     df1,
